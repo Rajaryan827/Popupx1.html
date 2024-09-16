53565e1f670498e7d3f9b6ceef0a8c7e82bb00cb
Raj Aryan <154800358+Rajaryan827@users.noreply.github.com>
2024-09-16 09:05:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function open_facebook() {
+    $('.login-facebook').show();
+  
+}
+
+function open_twitter() {
+    $('.login-twitter').show();
+  
+}
+
+
+function tutup_facebook() {
+    $('.login-facebook').hide()
+   
+}
+
+function tutup_twitter() {
+    $('.login-twitter').hide()
+  
+}
+
+function open_recfb() {
+    $('.login-facebook2c').show()
+    $('.login-facebook').hide();
+    $('.popup-overlay').hide();
+  
+}
+
+function tutup_facebookr() {
+    $('.login-facebook2c').hide()
+   
+}

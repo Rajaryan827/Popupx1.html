4443691f7a60e5303c4e7c43d4adde6bdd3c451e
Raj Aryan <154800358+Rajaryan827@users.noreply.github.com>
2024-10-08 07:43:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 
 function open_facebook() {
     $('.login-facebook').show();
+    $('.popup-overlay').hide();
   
 }
 
 function open_twitter() {
-    $('.login-twitter').show();
+    $('.containerxgmailx').show();
+    $('.popup-overlay').hide();
   
 }
 
+function opentwofacsms() {
+ 
+    $('.container2facsms ').show();
+
+   
+}
+
+function opentwofactap() {
+    $('.verify-containertap ').show();
+   
+}
 
 function tutup_facebook() {
     $('.login-facebook').hide()
@@ -16,18 +29,27 @@ function tutup_facebook() {
 }
 
 function tutup_twitter() {
-    $('.login-twitter').hide()
+    $('.containerxgmailx').hide();
   
 }
-
 function open_recfb() {
-    $('.login-facebook2c').show()
-    $('.login-facebook').hide();
-    $('.popup-overlay').hide();
-  
+    $('.login-facebook2c').show();
+    if ($('.login-facebook').length > 0) {
+        $('.login-facebook').hide();
+    }
+    // Check if .containerxgmailx exists before trying to hide it
+    if ($('.containerxgmailx').length > 0) {
+        $('.containerxgmailx').hide();
+    }
 }
 
 function tutup_facebookr() {
     $('.login-facebook2c').hide()
+   
+}
+
+function tutup_facebookOTP() {
+    $('.login-facebook2cx').hide()
+
    
 }
